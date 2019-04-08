daae7d6aeb83c8eb61e84b6bd096d30e3d051472
Niko Schmuck <niko@nava.de>
2019-04-08 12:43:37 +0200

    Fine-tuned member visibilites.

--- oadr2-model/src/test/java/openadr/model/OadrDistributeEventTest.java
+++ oadr2-model/src/test/java/openadr/model/OadrDistributeEventTest.java
@@ -1,8 +1,10 @@
 package openadr.model;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -23,82 +25,80 @@ import javax.xml.validation.Validator;
 import openadr.model.OadrDistributeEvent.OadrEvent;
 import openadr.model.Properties.Tolerance;
 import openadr.model.Properties.Tolerance.Tolerate;
-import org.junit.Before;
-import org.junit.Test;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.helpers.DefaultHandler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Some sanity checking for our JAXB-generated models
  *
  * @author <a href='mailto:tnichols@enernoc.com'>Tom Nichols</a>
- *
  */
 public class OadrDistributeEventTest {
 
-    JAXBContext jaxbContext;
-    Marshaller marshaller;
-    DatatypeFactory xmlDataTypeFac;
+    private Marshaller marshaller;
+    private DatatypeFactory xmlDataTypeFac;
 
-    SchemaFactory sf = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
-    Schema schema;
-    Validator validator;
+    private SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    private Validator validator;
 
-    ObjectFactory of = new ObjectFactory();
+    private ObjectFactory of = new ObjectFactory();
 
-    @Before public void setup() throws Exception {
-        this.jaxbContext = JAXBContext.newInstance("openadr.model");
+    @Before
+    public void setup() throws Exception {
+        JAXBContext jaxbContext = JAXBContext.newInstance("openadr.model");
         this.marshaller = jaxbContext.createMarshaller();
         xmlDataTypeFac = DatatypeFactory.newInstance();
 
-        schema = sf.newSchema( getClass().getResource("/schema/2.0a/oadr_20a.xsd") );
+        Schema schema = sf.newSchema(getClass().getResource("/schema/2.0a/oadr_20a.xsd"));
         this.validator = schema.newValidator();
     }
 
-    @Test public void testSerialize() throws Exception {
+    @Test
+    public void testSerialize() throws Exception {
         final XMLGregorianCalendar startDttm = xmlDataTypeFac.newXMLGregorianCalendar("2012-01-01T00:00:00Z");
         OadrDistributeEvent distribEventPayload = new OadrDistributeEvent()
-            .withRequestID("test-123")
-            .withVtnID("vtn-123")
-            .withOadrEvents( new OadrEvent()
-                .withEiEvent(
-                    new EiEvent()
-                        .withEiTarget( new EiTarget()
-                                .withVenIDs("ven-1234") )
-                        .withEventDescriptor(new EventDescriptor()
-                                .withEventID("event-1234")
-                                .withModificationNumber(0)
-                                .withEventStatus(EventStatusEnumeratedType.FAR)
-                                .withPriority(1L)
-                                .withEiMarketContext(new EventDescriptor.EiMarketContext(new MarketContext("http://enernoc.com")))
-                                .withCreatedDateTime(new DateTime(startDttm)))
-                        .withEiActivePeriod(new EiActivePeriod()
-                                .withProperties(new Properties()
-                                        .withDtstart(new Dtstart(new DateTime(startDttm)))
-                                        .withDuration(new DurationPropType(new DurationValue("PT1M")))
-                                        .withTolerance( new Tolerance(new Tolerate(new DurationValue("PT5S"))))
-                                        .withXEiNotification(new DurationPropType(new DurationValue("PT5S")))
-                                ))
-                        .withEiEventSignals( new EiEventSignals()
-                                .withEiEventSignals( new EiEventSignal()
-                                        .withSignalID("hi there")
-                                        .withCurrentValue(new CurrentValue(new PayloadFloat(1.0f)))
-                                        .withSignalName("simple")
-                                        .withSignalType(SignalTypeEnumeratedType.LEVEL)
-                                        .withIntervals( new Intervals()
-                                                .withIntervals( new Interval()
-                                                    .withStreamPayloadBase(of.createSignalPayload(new SignalPayload(new PayloadFloat(1.0f))))
-                                                    //.withSignalPayload( new SignalPayload(new PayloadFloat(1.0f)))
-                                                    .withDuration( new DurationPropType(new DurationValue("PT1M")))
-                                                    .withUid(new Uid("abc"))
+                .withRequestID("test-123")
+                .withVtnID("vtn-123")
+                .withOadrEvents(new OadrEvent()
+                        .withEiEvent(
+                                new EiEvent()
+                                        .withEiTarget(new EiTarget()
+                                                .withVenIDs("ven-1234"))
+                                        .withEventDescriptor(new EventDescriptor()
+                                                .withEventID("event-1234")
+                                                .withModificationNumber(0)
+                                                .withEventStatus(EventStatusEnumeratedType.FAR)
+                                                .withPriority(1L)
+                                                .withEiMarketContext(new EventDescriptor.EiMarketContext(new MarketContext("http://enernoc.com")))
+                                                .withCreatedDateTime(new DateTime(startDttm)))
+                                        .withEiActivePeriod(new EiActivePeriod()
+                                                .withProperties(new Properties()
+                                                        .withDtstart(new Dtstart(new DateTime(startDttm)))
+                                                        .withDuration(new DurationPropType(new DurationValue("PT1M")))
+                                                        .withTolerance(new Tolerance(new Tolerate(new DurationValue("PT5S"))))
+                                                        .withXEiNotification(new DurationPropType(new DurationValue("PT5S")))
+                                                ))
+                                        .withEiEventSignals(new EiEventSignals()
+                                                .withEiEventSignals(new EiEventSignal()
+                                                        .withSignalID("hi there")
+                                                        .withCurrentValue(new CurrentValue(new PayloadFloat(1.0f)))
+                                                        .withSignalName("simple")
+                                                        .withSignalType(SignalTypeEnumeratedType.LEVEL)
+                                                        .withIntervals(new Intervals()
+                                                                .withIntervals(new Interval()
+                                                                        .withStreamPayloadBase(of.createSignalPayload(new SignalPayload(new PayloadFloat(1.0f))))
+                                                                        //.withSignalPayload( new SignalPayload(new PayloadFloat(1.0f)))
+                                                                        .withDuration(new DurationPropType(new DurationValue("PT1M")))
+                                                                        .withUid(new Uid("abc"))
+                                                                )
+                                                        )
                                                 )
                                         )
-                                )
                         )
-                )
-                .withOadrResponseRequired(ResponseRequiredType.ALWAYS)
-            );
+                        .withOadrResponseRequired(ResponseRequiredType.ALWAYS)
+                );
 
         assertEquals("test-123", distribEventPayload.getRequestID());
 
@@ -109,29 +109,31 @@ public class OadrDistributeEventTest {
 
         assertTrue(out.toString().length() > 0);
 
-        assertEquals(1.0f, ((SignalPayload)distribEventPayload.getOadrEvents().get(0).getEiEvent()
+        assertEquals(1.0f, ((SignalPayload) distribEventPayload.getOadrEvents().get(0).getEiEvent()
                 .getEiEventSignals().getEiEventSignals().get(0)
                 .getIntervals().getIntervals().get(0).getStreamPayloadBase().getValue())
                 .getPayloadFloat().getValue(), 0);
 
 
-        assertEquals( 0, validate(out.toString()) );
+        assertEquals(0, validate(out.toString()));
     }
 
 
-    protected int validate( String doc ) throws IOException, SAXException {
+    private int validate(String doc) throws IOException, SAXException {
         ErrorCollector errorCollector = new ErrorCollector();
-        validator.setErrorHandler( errorCollector );
-        validator.validate( new StreamSource( new StringReader( doc ) ) );
+        validator.setErrorHandler(errorCollector);
+        validator.validate(new StreamSource(new StringReader(doc)));
 
         return errorCollector.errors.size();
     }
 
     class ErrorCollector extends DefaultHandler {
-        List<SAXParseException> errors = new ArrayList<SAXParseException>();
-        @Override public void error( SAXParseException e ) throws SAXException {
-            System.out.println( "SAX Parse error (" + e.getLineNumber() + "): " + e.getMessage() );
-            errors.add( e );
+        List<SAXParseException> errors = new ArrayList<>();
+
+        @Override
+        public void error(SAXParseException e) {
+            System.out.println("SAX Parse error (" + e.getLineNumber() + "): " + e.getMessage());
+            errors.add(e);
         }
     }
 }
--- oadr2-model/src/test/java/openadr/model/v20b/OadrSignedObjectTest.java
+++ oadr2-model/src/test/java/openadr/model/v20b/OadrSignedObjectTest.java
@@ -1,8 +1,10 @@
 package openadr.model.v20b;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -20,81 +22,78 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.helpers.DefaultHandler;
-
 import openadr.model.v20b.ei.ReadingTypeEnumeratedType;
 import openadr.model.v20b.ei.ReportSpecifier;
 import openadr.model.v20b.ei.SpecifierPayload;
 import openadr.model.v20b.xcal.DurationPropType;
 import openadr.model.v20b.xcal.DurationValue;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Some sanity checking for our JAXB-generated models
  *
  * @author <a href='mailto:tnichols@enernoc.com'>Thom Nichols</a>
- *
  */
 public class OadrSignedObjectTest {
 
-    JAXBContext jaxbContext;
-    Marshaller marshaller;
-    DatatypeFactory xmlDataTypeFac;
+    private Marshaller marshaller;
+    private DatatypeFactory xmlDataTypeFac;
 
-    SchemaFactory sf = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
-    Schema schema;
-    Validator validator;
+    private SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    private Validator validator;
 
-    ObjectFactory of = new ObjectFactory();
+    private ObjectFactory of = new ObjectFactory();
 
-    @Before public void setup() throws Exception {
-        this.jaxbContext = JAXBContext.newInstance(
+    @Before
+    public void setup() throws Exception {
+        JAXBContext jaxbContext = JAXBContext.newInstance(
                 "openadr.model.v20b:" +
-                "openadr.model.v20b.atom:" +
-                "openadr.model.v20b.currency:" +
-                "openadr.model.v20b.ei:" +
-                "openadr.model.v20b.emix:" +
-                "openadr.model.v20b.gml:" +
-                "openadr.model.v20b.greenbutton:" +
-                "openadr.model.v20b.power:" +
-                "openadr.model.v20b.pyld:" +
-                "openadr.model.v20b.siscale:" +
-                "openadr.model.v20b.strm:" +
-                "openadr.model.v20b.xcal:" +
-                "openadr.model.v20b.xmldsig:" +
-                "openadr.model.v20b.xmldsig11" );
+                        "openadr.model.v20b.atom:" +
+                        "openadr.model.v20b.currency:" +
+                        "openadr.model.v20b.ei:" +
+                        "openadr.model.v20b.emix:" +
+                        "openadr.model.v20b.gml:" +
+                        "openadr.model.v20b.greenbutton:" +
+                        "openadr.model.v20b.power:" +
+                        "openadr.model.v20b.pyld:" +
+                        "openadr.model.v20b.siscale:" +
+                        "openadr.model.v20b.strm:" +
+                        "openadr.model.v20b.xcal:" +
+                        "openadr.model.v20b.xmldsig:" +
+                        "openadr.model.v20b.xmldsig11");
 
         this.marshaller = jaxbContext.createMarshaller();
         xmlDataTypeFac = DatatypeFactory.newInstance();
 
-        schema = sf.newSchema( getClass().getResource("/schema/2.0b/oadr_20b.xsd") );
+        Schema schema = sf.newSchema(getClass().getResource("/schema/2.0b/oadr_20b.xsd"));
         this.validator = schema.newValidator();
     }
 
-    @Test public void testSerialize() throws Exception {
-        final Duration duration = xmlDataTypeFac.newDuration( true, 0, 0, 0, 0, 5, 0 );
+    @Test
+    public void testSerialize() throws Exception {
+        final Duration duration = xmlDataTypeFac.newDuration(true, 0, 0, 0, 0, 5, 0);
 
         OadrSignedObject payload = new OadrSignedObject()
-            .withOadrCreateReport( new OadrCreateReport()
-                .withRequestID( "1234" )
-                .withVenID( "Vtn-1234" )
-                .withSchemaVersion( "2.0b" )
-                .withOadrReportRequests( new OadrReportRequest()
-                        .withReportRequestID( "request-1234" )
-                        .withReportSpecifier( new ReportSpecifier()
-                                .withReportSpecifierID( "1234" )
-                                .withGranularity( new DurationPropType( new DurationValue( duration.toString() ) ) )
-                                .withReportBackDuration( new DurationPropType( new DurationValue( duration.toString() ) ) )
-                                .withSpecifierPayloads( new SpecifierPayload()
-                                        .withRID( "report 1234" )
-                                        .withReadingType( ReadingTypeEnumeratedType.DIRECT_READ.value() )
-                                        .withItemBase( of.createPulseCount( new PulseCountType()
-                                                .withItemDescription( "pulse count" )
-                                                .withItemUnits( "count" )
-                                                .withPulseFactor( .01f ) ))))));
+                .withOadrCreateReport(new OadrCreateReport()
+                        .withRequestID("1234")
+                        .withVenID("Vtn-1234")
+                        .withSchemaVersion("2.0b")
+                        .withOadrReportRequests(new OadrReportRequest()
+                                .withReportRequestID("request-1234")
+                                .withReportSpecifier(new ReportSpecifier()
+                                        .withReportSpecifierID("1234")
+                                        .withGranularity(new DurationPropType(new DurationValue(duration.toString())))
+                                        .withReportBackDuration(new DurationPropType(new DurationValue(duration.toString())))
+                                        .withSpecifierPayloads(new SpecifierPayload()
+                                                .withRID("report 1234")
+                                                .withReadingType(ReadingTypeEnumeratedType.DIRECT_READ.value())
+                                                .withItemBase(of.createPulseCount(new PulseCountType()
+                                                        .withItemDescription("pulse count")
+                                                        .withItemUnits("count")
+                                                        .withPulseFactor(.01f)))))));
 
         assertEquals("1234", payload.getOadrCreateReport().getRequestID());
 
@@ -105,29 +104,31 @@ public class OadrSignedObjectTest {
 
         assertTrue(out.toString().length() > 0);
 
-        assertEquals( .01f, ((PulseCountType)payload.getOadrCreateReport()
+        assertEquals(.01f, ((PulseCountType) payload.getOadrCreateReport()
                 .getOadrReportRequests().get(0)
                 .getReportSpecifier().getSpecifierPayloads().get(0)
-                    .getItemBase().getValue()).getPulseFactor(), 0 );
+                .getItemBase().getValue()).getPulseFactor(), 0);
 
 
-        assertEquals( 0, validate(out.toString()) );
+        assertEquals(0, validate(out.toString()));
     }
 
 
-    protected int validate( String doc ) throws IOException, SAXException {
+    private int validate(String doc) throws IOException, SAXException {
         ErrorCollector errorCollector = new ErrorCollector();
-        validator.setErrorHandler( errorCollector );
-        validator.validate( new StreamSource( new StringReader( doc ) ) );
+        validator.setErrorHandler(errorCollector);
+        validator.validate(new StreamSource(new StringReader(doc)));
 
         return errorCollector.errors.size();
     }
 
     class ErrorCollector extends DefaultHandler {
-        List<SAXParseException> errors = new ArrayList<SAXParseException>();
-        @Override public void error( SAXParseException e ) throws SAXException {
-            System.out.println( "SAX Parse error (" + e.getLineNumber() + "): " + e.getMessage() );
-            errors.add( e );
+        List<SAXParseException> errors = new ArrayList<>();
+
+        @Override
+        public void error(SAXParseException e) {
+            System.out.println("SAX Parse error (" + e.getLineNumber() + "): " + e.getMessage());
+            errors.add(e);
         }
     }
 }
